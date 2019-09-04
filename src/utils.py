e619ef8d2797c5eface98781241e90450111bea4
Paula Malinauskas <malinauskaspaula@gmail.com>
2019-09-04 19:49:00 -0300

    agrego la funcion show_df

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,11 @@ import pandas as pd
 def hello_world():
     print("")
     print("hello world!")
+    
+    
+    
+def show_df(df):
+    
+    print(df.shape)
+    display(df.head())
+    display(df.describe())
